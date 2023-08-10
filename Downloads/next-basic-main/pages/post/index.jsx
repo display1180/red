c171772fdd144c561b16733af0d54a4464806043
nikji <display1180@gmail.com>
2023-08-10 10:42:44 +0900

    a

--- Downloads/next-basic-main/pages/post/index.jsx
+++ Downloads/next-basic-main/pages/post/index.jsx
@@ -1,11 +1,16 @@
 import SubLayout from '@/components/SubLayout';
+import axios from 'axios';
+import { useEffect } from 'react';
 
-function post() {
+function Post() {
+	useEffect(() => {
+		axios.get('/api/post').then((res) => console.log(res));
+	}, []);
 	return (
 		<SubLayout>
-			<p>포스트 페이지 인트로 화면입니다.</p>
+			<p>포스트페이지 인트로화면입니다.</p>
 		</SubLayout>
 	);
 }
 
-export default post;
+export default Post;
